8da61032dfe80c782d183eb12af816a13014cd47
Trinhanhphung <anhphungtrinh2005@gmail.com>
2024-10-07 17:37:31 +0700

    Add project files.

---
+++ Project_Booking_Ticket/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+
+namespace Project_Booking_Ticket.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult DangKy_DangNhap()
+        {
+            return View();
+        }
+        public ActionResult Dich_Vu_Quang_Cao()
+        {
+            return View();
+        }
+        public ActionResult Ve_Chung_Toi()
+        {
+            return View();
+        }
+        public ActionResult Khuyen_Mai()
+        {
+            return View();
+        }
+        public ActionResult He_Thong_Rap()
+        {
+            return View();
+        }
+        public ActionResult Lich_chieu_rap()
+        {
+            return View();
+        }
+        public ActionResult Lich_chieu_phim()
+        {
+            return View();
+        }
+        public ActionResult Layout_Home_Page_Main()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Project_Booking_Ticket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_Booking_Ticket.MvcApplication" Language="C#" %>
